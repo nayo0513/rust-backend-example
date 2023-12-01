43775fa21a1968453c320b6dde5c2eb8bec3d006
nayo0513 <nayo05130513@gmail.com>
2023-12-01 12:18:21 +0900

    Add tests.

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 psql:
 	docker exec -it pocket-change-problem-db-1 psql -U postgres -h localhost -p 5432
+test:
+# Errors occur when it executes the tests in parallel.
+# https://github.com/launchbadge/sqlx/issues/2631
+	docker compose exec backend cargo test -- --test-threads=1
--- src/models/message.rs
+++ src/models/message.rs
@@ -42,7 +42,7 @@ impl MessageModel {
         // Check if parent_id exists.
         if parent_id.is_some()
             && query!(
-            r#"
+                r#"
             select id
             from message
             where id = $1
@@ -159,8 +159,9 @@ impl MessageModel {
         {
             return Err(Error::msg("User not found."));
         }
-        
-        let rows=if start_time.is_some() && end_time.is_some() {
+
+        let rows = if start_time.is_some() && end_time.is_some() {
+            // If both start_time and end_time are specified.
             query_as!(
                 MessageModel,
                 r#"
@@ -176,6 +177,7 @@ impl MessageModel {
             .fetch_all(pool)
             .await?
         } else if start_time.is_some() {
+            // If only start_time is specified.
             query_as!(
                 MessageModel,
                 r#"
@@ -190,6 +192,7 @@ impl MessageModel {
             .fetch_all(pool)
             .await?
         } else if end_time.is_some() {
+            // If only end_time is specified.
             query_as!(
                 MessageModel,
                 r#"
@@ -204,6 +207,7 @@ impl MessageModel {
             .fetch_all(pool)
             .await?
         } else {
+            // If both start_time and end_time are not specified.
             query_as!(
                 MessageModel,
                 r#"
@@ -218,5 +222,185 @@ impl MessageModel {
         };
 
         Ok(rows)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::models::message::MessageModel;
+    use anyhow::Result;
+    use sqlx::{query, query_as, PgPool};
+
+    #[sqlx::test]
+    async fn create(pool: PgPool) -> Result<()> {
+        let user_id = 1;
+        let message = "test message".to_string();
+        let parent_id = None;
+
+        // Create user.
+        query!(
+            r#"
+            insert into users (id, name, password, email)
+            values ($1, $2, $3, $4)
+            "#,
+            user_id,
+            "test",
+            "test",
+            "test@example.com"
+        )
+        .execute(&pool)
+        .await?;
+        // Create message.
+        let row = MessageModel::create(user_id, message.clone(), parent_id, &pool).await?;
+
+        assert_eq!(row.user_id, user_id);
+        assert_eq!(row.message, message);
+        assert_eq!(row.parent_id, parent_id);
+
+        Ok(())
+    }
+
+    #[sqlx::test]
+    async fn modify(pool: PgPool) -> Result<()> {
+        let user_id = 1;
+        let message = "test message".to_string();
+
+        // Create user.
+        query!(
+            r#"
+            insert into users (id, name, password, email)
+            values ($1, $2, $3, $4)
+            "#,
+            user_id,
+            "test",
+            "test",
+            "test@example.com"
+        )
+        .execute(&pool)
+        .await?;
+        // Create message.
+        query!(
+            r#"
+            insert into message (user_id, message, message_time)
+            values ($1, $2, now())
+            "#,
+            user_id,
+            message.clone(),
+        )
+        .execute(&pool)
+        .await?;
+
+        let modified_message = "modified message".to_string();
+        // Modify message.
+        let row = MessageModel::modify(1, modified_message.clone(), &pool).await?;
+
+        assert_eq!(row.message, modified_message);
+        Ok(())
+    }
+
+    #[sqlx::test]
+    async fn delete(pool: PgPool) -> Result<()> {
+        let user_id = 1;
+        let message = "test message".to_string();
+
+        // Create user.
+        query!(
+            r#"
+            insert into users (id, name, password, email)
+            values ($1, $2, $3, $4)
+            "#,
+            user_id,
+            "test",
+            "test",
+            "test@example.com"
+        )
+        .execute(&pool)
+        .await?;
+        // Create message.
+        query!(
+            r#"
+            insert into message (user_id, message, message_time)
+            values ($1, $2, now())
+            "#,
+            user_id,
+            message.clone(),
+        )
+        .execute(&pool)
+        .await?;
+
+        // Delete message.
+        MessageModel::delete(user_id, &pool).await?;
+
+        // Check if message deleted.
+        let row = query_as!(
+            MessageModel,
+            r#"
+            select *
+            from message
+            "#
+        )
+        .fetch_all(&pool)
+        .await?;
+        assert_eq!(row.len(), 0);
+
+        Ok(())
+    }
+
+    #[sqlx::test]
+    async fn find_by_user_id_and_time_range(pool: PgPool) -> Result<()> {
+        let user_id = 1;
+        let message = "test message".to_string();
+
+        // Create user.
+        query!(
+            r#"
+            insert into users (id, name, password, email)
+            values ($1, $2, $3, $4)
+            "#,
+            user_id,
+            "test",
+            "test",
+            "test@example.com"
+        )
+        .execute(&pool)
+        .await?;
+        // Create message.
+        let message_time = chrono::Utc::now();
+        query!(
+            r#"
+            insert into message (user_id, message, message_time)
+            values ($1, $2, $3)
+            "#,
+            user_id,
+            message.clone(),
+            message_time
+        )
+        .execute(&pool)
+        .await?;
+
+        // Find message.
+        // Find by user_id.
+        let rows = MessageModel::find_by_user_id_and_time_range(user_id, None, None, &pool).await?;
+        assert_eq!(rows.len(), 1);
+        // Find by user_id and start_time.
+        let rows =
+            MessageModel::find_by_user_id_and_time_range(user_id, Some(message_time), None, &pool)
+                .await?;
+        assert_eq!(rows.len(), 1);
+        // Find by user_id and end_time.
+        let rows =
+            MessageModel::find_by_user_id_and_time_range(user_id, None, Some(message_time), &pool)
+                .await?;
+        assert_eq!(rows.len(), 1);
+        // Find by user_id and start_time and end_time.
+        let rows = MessageModel::find_by_user_id_and_time_range(
+            user_id,
+            Some(message_time),
+            Some(message_time),
+            &pool,
+        )
+        .await?;
+        assert_eq!(rows.len(), 1);
+        Ok(())
     }
 }
